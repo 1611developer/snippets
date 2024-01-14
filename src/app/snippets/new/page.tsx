29caa72175b3149bd398b5a818c7d566ed94bf69
Philip <1611.developer@gmail.com>
2024-01-13 19:22:09 -0800

    started async

--- src/app/snippets/new/page.tsx
+++ src/app/snippets/new/page.tsx
@@ -1,35 +1,43 @@
+import { db } from '@/db'
 
 export default function SnippetCreatePage() {
- return <form>
-   <h3 className="font-bold m-3">Create a Snippet</h3>
-   <div className="flex flex-col gap-4">
-     <div className=" flex gap-4">
-       <lable className="w-12" htmlFor="title">
-         Title
-       </lable>
-       <input
-         name="title"
-	       className="border rounded p-2 w-full"
-	       id="title"
-       />
-     </div>
+ async function createSnippet() {
+   // This needs to be a server function.
+   
+   // Check the user's inputs and make sure they're valid.
+   
+   // Create a new record in the database.
 
-     <div className=" flex gap-4">
-       <lable className="w-12" htmlFor="code">
-         Code
-       </lable>
-       <textarea
-         name="code"
-         className="border rounded p-2 w-full"
-         id="code"
-       />
-     </div>
+ }
 
-     <button type="submit" className="rounded p-2 bg-blue-200">
-      submit
-     </button>
-
-   </div>
- </form> 
+return
+  <form>
+    <h3 className="font-bold m-3">Create a Snippet</h3>
+    <div className="flex flex-col gap-4">
+      <div className=" flex gap-4">
+        <lable className="w-12" htmlFor="title">
+          Title
+        </lable>
+        <input
+        name="title"
+        className="border rounded p-2 w-full"
+	      id="title"
+        />
+      </div>
+      <div className="flex gap-4">
+        <lable className="w-12" htmlFor="code">
+          Code
+        </lable>
+        <textarea
+          name="code"
+          className="border rounded p-2 w-full"
+          id="code"
+        />
+      </div>
+      <button type="submit" className="rounded p-2 bg-blue-200">
+        submit
+      </button>
+    </div>
+  </form> 
 }
 
